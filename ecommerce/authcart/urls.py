f9de9728fadbc0e9b6ab8c0d99a89e236dec1fe0
Nipun-Pratap-Singh <nipunpratap86300@gmail.com>
2024-05-06 03:45:30 +0530

    1st commit

---
+++ ecommerce/authcart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from authcart import views
+
+urlpatterns = [
+
+    path('signup/',views.signup,name="signup"),
+    path('login/',views.handlelogin,name="handlelogin"),
+    path('logout/',views.handlelogout,name="handlelogout"),
+    path('activate/<uidb64>/<token>',views.ActivateAccountView.as_view(),name = 'activate')
+    
+    
+]
---
+++ ecommerce/ecommerceapp/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from ecommerceapp import views
+
+
+urlpatterns = [
+   
+   path('',views.index,name="index"),
+   path('contact',views.contact,name="contact"),
+   path('about',views.about,name="about"),
+   
+]
+
+
+
+
+
+# from django.urls import path
+# from ecommerceapp import views
+
+# urlpatterns = [
+#     path('',views.index,name="index"),
+#     path('contact',views.contact,name="contact"),
+#     path('about',views.about,name="about"),
+#     path('profile',views.profile,name="profile"),
+#     path('checkout/', views.checkout, name="Checkout"),
+#     path('handlerequest/', views.handlerequest, name="HandleRequest"),
+
+# ]
